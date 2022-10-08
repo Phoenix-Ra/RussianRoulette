6bb0b16b1589dea6cf92a7f92f5a190ca5041216
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2022-10-08 17:09:45 +0200

    symbol colorize

--- src/main/java/me/phoenixra/russian_roulette/utils/GameSymbols.java
+++ src/main/java/me/phoenixra/russian_roulette/utils/GameSymbols.java
@@ -1,8 +1,10 @@
 package me.phoenixra.russian_roulette.utils;
 
+import me.phoenixra.core.PhoenixUtils;
+
 public class GameSymbols {
 
-    public static String shoot(){
-        return "\u26a1";
+    public static String shoot(String prefix){
+        return PhoenixUtils.colorFormat(prefix)+"\u26a1";
     }
 }
